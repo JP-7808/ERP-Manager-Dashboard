3e51aa4d7c4b27669945693fe420b3d87bfb878e
Jay Prakash <jayp7808@gmail.com>
2025-03-23 16:03:01 +0530

    Updated ui of salestable

--- src/ManagerDashPages/SalesTable.jsx
+++ src/ManagerDashPages/SalesTable.jsx
@@ -43,7 +43,7 @@ const SalesTable = () => {
   const paginatedData = salesData.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage);
 
   return (
-    <div className="p-6 bg-white rounded-lg shadow-lg">
+    <div className="p-4 bg-white rounded-lg shadow-lg">
       <h3 className="text-lg font-semibold mb-4">Sales Report</h3>
 
       {loading ? (
